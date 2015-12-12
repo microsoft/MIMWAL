2d921bdc8ae453cb04c33b41941ef8bb64f29432
Nilesh Ghodekar <nilesh.ghodekar@outlook.com>
2015-12-12 04:31:32 +0000

    Updated file headers.

--- src/WorkflowActivityLibrary/Activities/SendEmailNotification.cs
+++ src/WorkflowActivityLibrary/Activities/SendEmailNotification.cs
@@ -1,7 +1,6 @@
 ﻿//-----------------------------------------------------------------------------------------------------------------------
 // <copyright file="SendEmailNotification.cs" company="Microsoft">
 //      Copyright (c) Microsoft. All Rights Reserved.
-//      Microsoft makes no warranties, expressed or implied.
 //      Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 // </copyright>
 // <summary>
