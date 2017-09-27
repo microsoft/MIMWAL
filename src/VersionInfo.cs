7a809906553c79d806484b4460f05f5fa509491f
NileshGhodekar <nilesh.ghodekar@outlook.com>
2017-09-27 10:02:39 +0100

    BUG fix in parameter validation of DateTimeFromString. BUG fix in SortList function return value.

--- src/VersionInfo.cs
+++ src/VersionInfo.cs
@@ -22,7 +22,7 @@ namespace MicrosoftServices.IdentityManagement.WorkflowActivityLibrary
         ///      Build Number (MMDD)
         ///      Revision (if any on the same day)
         /// </summary>
-        internal const string Version = "2.17.0716.0";
+        internal const string Version = "2.17.0927.0";
 
         /// <summary>
         /// File Version information for the assembly consists of the following four values:
@@ -31,6 +31,6 @@ namespace MicrosoftServices.IdentityManagement.WorkflowActivityLibrary
         ///      Build Number (MMDD)
         ///      Revision (if any on the same day)
         /// </summary>
-        internal const string FileVersion = "2.17.0716.0";
+        internal const string FileVersion = "2.17.0927.0";
     }
 }
